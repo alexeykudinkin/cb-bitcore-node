32614341e8a2a9894d42c02e52b1acf5cd721507
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2016-01-09 04:03:43 +0300

    (MISC): Fixed spurious slashes inside URLs

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ var Transactions  = require('./transactions');
 var utils         = require('./utils');
 
 function Bitcore(baseUrl) {
-  this.transactions = new Transactions  (baseUrl + '/cb/transactions/');
-  this.addresses    = new Addresses     (baseUrl + '/cb/addresses/',  this.transactions);
-  this.blocks       = new Blocks        (baseUrl + '/cb/blocks/',     this.transactions);
+  this.transactions = new Transactions  (baseUrl + '/cb/transactions');
+  this.addresses    = new Addresses     (baseUrl + '/cb/addresses',  this.transactions);
+  this.blocks       = new Blocks        (baseUrl + '/cb/blocks',     this.transactions);
 }
 
 Bitcore.throttleGet = utils.throttleGet;
